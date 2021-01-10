23812b19a1ad7ea63be3ae0f33b53a7ad0ea9c35
Carlo Ferrer <thecarloferrer@gmail.com>
2021-01-10 14:23:35 -0500

    Remove SECRET_KEY
    Guess I'll have to add a new one later lol.

--- pantry_backend/settings.py
+++ pantry_backend/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'g9)^s$r!%x7#2_r6@1e_qaupvhyv^0dngbwsz*ow&0+a25)a6%'
+SECRET_KEY = 'add-a-real-secret-key-later'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
