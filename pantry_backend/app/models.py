16cfe66233d93bf5c225857a0503ff49e2b9af2b
Carlo Ferrer <thecarloferrer@gmail.com>
2021-01-13 22:17:42 -0500

    Add instance methods to get expiry
    Still need to add tests.

--- pantry_backend/app/models.py
+++ pantry_backend/app/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.humanize.templatetags.humanize import naturalday
+from django.utils import timezone
 
 
 class Location(models.Model):
@@ -12,9 +14,17 @@ class Location(models.Model):
 class Consumable(models.Model):
     name = models.CharField(max_length=48)
     description = models.TextField()
-    count = models.IntegerField()
-    expiry = models.DateField()
-    locations = models.ManyToManyField(Location)
+    count = models.IntegerField(default=1)
+    expiry = models.DateField(blank=True)
+    locations = models.ManyToManyField(Location, related_name='consumables')
 
     def __str__(self):
         return self.name
+
+    def get_expiry_human_date(self):
+        return naturalday(self.expiry)
+
+    def get_days_to_expiry(self):
+        now = timezone.now().date()
+        days_to_expiry = (self.expiry - now).days
+        return days_to_expiry
